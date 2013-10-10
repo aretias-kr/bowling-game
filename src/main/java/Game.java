c10e39a5809ba3a8fe8a082c0169a5d1a3d633c1
JeongInn Lee <aretias.kr@gmail.com>
2013-10-10 12:01:03 +0900

    Add completed test case for bowling game

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,11 +2,43 @@
  * @author JeongInn
  */
 public class Game {
-	private int score = 0;
+	private int  rolls[] = new int[21];
+	private int currentRoll = 0;
 	public void roll(int pins) {
-		score += pins;
+		rolls[currentRoll++] = pins;
 	}
 	public int score() {
+		int score = 0;
+		int rollIndex = 0;
+		for (int frame = 0; frame < 10; frame++) {
+			if (isStrike(rollIndex)) {
+				score += 10 + strikeBonus(rollIndex);
+				rollIndex += 1;
+			} else if (isSpare(rollIndex)) {
+				score += 10 + spareBonus(rollIndex);
+				rollIndex += 2;
+			} else {
+				score += scoreInFrame(rollIndex);
+				rollIndex += 2;
+			}
+		}
 		return score;
+	}
+	private int strikeBonus(int rollIndex) {
+		return rolls[rollIndex + 1] + rolls[rollIndex + 2];
+	}
+	private boolean isStrike(int rollIndex) {
+		return rolls[rollIndex] == 10;
+	}
+	
+	public int spareBonus(int rollIndex) {
+		return rolls[rollIndex + 2];
+	}
+	private boolean isSpare(int rollIndex) {
+		return rolls[rollIndex] + rolls[rollIndex + 1] == 10;
+	}
+	
+	public int scoreInFrame(int rollIndex) {
+		return rolls[rollIndex] + rolls[rollIndex + 1];
 	}
 }
--- src/test/java/BowlingGameTest.java
+++ src/test/java/BowlingGameTest.java
@@ -1,7 +1,6 @@
 import static org.junit.Assert.*;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 /**
@@ -27,15 +26,47 @@ public class BowlingGameTest {
 		assertEquals(20, game.score());
 	}
 	
-	@Ignore("Until we get design right")
 	@Test
 	public void should_return_16_when_one_spare_and_3_points() throws Exception {
-		game.roll(5);
-		game.roll(5);
+		rollSpare();
 		game.roll(3);
 		
 		rollMany(17, 0);
 		assertEquals(16, game.score());
+	}
+
+	private void rollSpare() {
+		game.roll(5);
+		game.roll(5);
+	}
+	
+	@Test
+	public void should_return_24_when_one_strike_and_3_and_4_points() throws Exception {
+		rollStrike();
+		game.roll(3);
+		game.roll(4);
+		
+		rollMany(16, 0);
+		
+		assertEquals(24, game.score());
+	}
+	
+	@Test
+	public void should_return_300_when_perfect_game() throws Exception {
+		rollMany(12, 10);
+		assertEquals(300, game.score());
+	}
+
+	@Test
+	public void should_return_30_when_two_strikes() throws Exception {
+		rollStrike();
+		rollStrike();
+		rollMany(16, 0);
+		assertEquals(30, game.score());
+	}
+	
+	private void rollStrike() {
+		game.roll(10);
 	}
 	
 	private void rollMany(int nRolls, int pins) {
